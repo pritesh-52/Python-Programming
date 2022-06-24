f2aa92b99675b383dc112ab7d093cb8f3afeb59d
Pritesh Bhatiya <priteshbhatiya52@gmail.com>
2022-06-24 12:04:09 +0530

    Basic programms

---
+++ Basic/swap_of.py
@@ -0,0 +1,13 @@
+#Swap of two number
+
+a=int(input("Enter Number"))  #user input
+b=int(input("Enter Number"))
+
+print("Before Swap of a",a)
+print("Before Swap of b",b)
+
+c=a
+a=b    #swapping
+b=c
+print("After swap of a",a)    #print swapping
+print("After Swap of b",b)
