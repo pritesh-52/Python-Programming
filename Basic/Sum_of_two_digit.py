c32f3ca142da2a84419a698808786e3567e16cb8
Pritesh Bhatiya <priteshbhatiya52@gmail.com>
2022-06-24 12:01:09 +0530

    Basic programms

---
+++ Basic/Sum_of_two_digit.py
@@ -0,0 +1,9 @@
+#SUM OF TWO DIGIT TWO NUMBER
+
+a=int(input("Enter Number"))   #User Input
+b=int(input("Enter Number"))
+print(a)   #print a
+print(b)   #print b
+c=a+b    #calculate
+print("Total Sum is",c) #print
+
